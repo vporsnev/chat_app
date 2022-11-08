6420403a23e81b7dfb084d268bf4a15e99d5cf15
vporsnev <v.porshnev@gmail.com>
2022-11-08 13:16:28 +0100

    asyncStorage an offline

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import NetInfo from '@react-native-community/netinfo';
 // import firebase from 'firebase';
 // import firestore from 'firebase';
 
@@ -18,7 +20,7 @@ export default class Chat extends React.Component {
         avatar: '',
         name: '',
       },
-      loggedInText: 'Please wait, you are getting logged in',
+      isConnected: false,
     }
 
   const firebaseConfig = {
@@ -36,6 +38,37 @@ export default class Chat extends React.Component {
     }
   
   this.referenceChatMessages = firebase.firestore().collection("messages");
+}
+
+async getMessages() {
+  let messages = '';
+  try {
+    messages = (await AsyncStorage.getItem('messages')) || [];
+    this.setState({
+      messages: JSON.parse(messages),
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async saveMessages() {
+  try {
+    await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async deleteMessages() {
+  try {
+    await AsyncStorage.removeItem('messages');
+    this.setState({
+      messages: []
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 onCollectionUpdate = (querySnapshot) => {
@@ -76,8 +109,20 @@ onSend(messages = []) {
      messages: GiftedChat.append(previousState.messages, messages),
      }),() => {
        this.addMessage();
+       this.saveMessages();
     });
 } 
+
+renderInputToolbar(props) {
+  if (this.state.isConnected == false) {
+  } else {
+    return(
+      <InputToolbar
+      {...props}
+      />
+    );
+  }
+}
 
 renderBubble(props) {
    return (
@@ -103,6 +148,7 @@ render() {
         <GiftedChat
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
+          renderInputToolbar={this.renderInputToolbar.bind(this)}
           onSend={messages => this.onSend(messages)}
           user={{
             _id: this.state.user._id,
@@ -118,28 +164,40 @@ render() {
   componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
-    // creating a references to messages collection
-    this.referenceChatMessages = firebase.firestore().collection('messages');
-    // listen to authentication events
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async(user) => {
-      if (!user) {
-        await
-        firebase.auth().signInAnonymously();
+
+    NetInfo.fetch().then((connection) => {
+      if (connection.isConnected) {
+        this.setState({ isConnected: true });
+        console.log('online');
+
+        this.referenceChatMessages = firebase
+          .firestore()
+          .collection('messages');
+
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+          if (!user) {
+            firebase.auth().signInAnonymously();
+          }
+          this.setState({
+            uid: user.uid,
+            messages: [],
+            user: {
+              _id: user.uid,
+              name: name,
+            },
+          });
+          this.unsubscribe = this.referenceChatMessages
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(this.onCollectionUpdate);
+        });
+      } else {
+        this.setState({ isConnected: false });
+        console.log('offline');
+
+        this.getMessages();
       }
-      this.setState({
-        uid: user.uid,
-        loggedInText: 'Hello there',
-        messages: [],
-        user: {
-          _id: user.uid,
-          name: name,
-        }
-      });
-      this.unsubscribe = this.referenceChatMessages
-        .orderBy("createdAt", "desc")
-        .onSnapshot(this.onCollectionUpdate);
     });
-    }
+  }
 
     componentWillUnmount() {
       this.unsubscribe();
